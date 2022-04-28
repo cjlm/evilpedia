b1b7715c20d93aeaf7abd4231e8be22624baca0b
selfire1 <try2banonym@gmail.com>
2022-04-28 15:25:00 +0200

    parse rss

--- src/_data/bwFeed.js
+++ src/_data/bwFeed.js
@@ -1,13 +1,17 @@
 const { parse } = require('rss-to-json');
-// async await
-(async () => {
+//// async await
+//(async () => {
+//
+//    var rss = await parse('https://blog.ethereum.org/feed.xml');
+//
+//    console.log(JSON.stringify(rss, null, 3));
+//
+//})();
 
-    var rss = await parse('https://blog.ethereum.org/feed.xml');
+module.exports = async function () {
 
+    var url = "https://bookworm.fm/feed/podcast/";
+    var rss = await parse(url);
     console.log(JSON.stringify(rss, null, 3));
-
-})();
-
-async function name(params) {
-    
+ return JSON.stringify(rss, null, 3);
 }
