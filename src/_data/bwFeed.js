c5ed15acad4894530a35be82dfa2914141c8a3af
selfire1 <try2banonym@gmail.com>
2022-05-06 13:26:10 +0200

    return title and no via regex

--- src/_data/bwFeed.js
+++ src/_data/bwFeed.js
@@ -1,18 +1,23 @@
 const { parse } = require('rss-to-json');
-//// async await
-//(async () => {
-//
-//    var rss = await parse('https://blog.ethereum.org/feed.xml');
-//
-//    console.log(JSON.stringify(rss, null, 3));
-//
-//})();
 
 module.exports = async function () {
+    var titleReg = /^(\d+).*?(\w+.+)/g;
 
     var url = "https://bookworm.fm/feed/podcast/";
     var rss = await parse(url);
-    //console.log(JSON.stringify(rss, null, 3));
- let feedObj = JSON.stringify(rss, null, 3);
- return rss.items[0].title;
+    //let feedObj = JSON.stringify(rss, null, 3);
+
+    let myArr = [];
+
+    for (let i = 0; i < rss.items.length; i++) {
+        let str = rss.items[i].title;
+        const matches = str.matchAll(titleReg);
+        for (const match of matches) {
+            const epNo = match[1]
+            const title = match[2]
+            myArr.push(epNo);
+            myArr.push(title);
+        }
+    }
+    return myArr;
 }
