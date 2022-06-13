07c11ccb9270c6b2f319107adda1188a50fbc176
selfire1 <try2banonym@gmail.com>
2022-06-13 15:59:56 +0200

    return trivia cards

--- src/_data/bwFeed.js
+++ src/_data/bwFeed.js
@@ -34,12 +34,12 @@ module.exports = async function () {
         // Undefined handling. This should be easier with .? but somehow eleventy doesn't like it
         var ratingM = contentStr.match(mikeReg) || false;
         var ratingJ = contentStr.match(joeReg) || false;
-        if (ratingM != false) { ratingM = ratingM[1];};
-        if (ratingJ != false) { ratingJ = ratingJ[1];};
+        if (ratingM != false) { ratingM = ratingM[1]; };
+        if (ratingJ != false) { ratingJ = ratingJ[1]; };
 
         obj.ratingMike = ratingM;
         obj.ratingJoe = ratingJ;
-        
+
         // Deal with episode without rating
         if (obj.ratingMike != false) {
             // Turn numbers into star icons
@@ -55,27 +55,27 @@ module.exports = async function () {
             obj.ratingJoe = "";
         }
         // Calculate combined rating
-        if (obj.rating) { obj.ratingAve = (parseFloat(obj.ratingJoe) + parseFloat(obj.ratingMike))/2};
+        if (obj.rating) { obj.ratingAve = (parseFloat(obj.ratingJoe) + parseFloat(obj.ratingMike)) / 2 };
         // Append '.0' to full ratings
         obj.ratingAve = appendZeroes(obj.ratingAve);
         // Push episode object to array
         if (obj.no != undefined) {
-           epArr.push(obj); 
-        } 
+            epArr.push(obj);
+        }
     }
 
     // Font Awesome star function
     function appendZeroes(rating) {
         if (/^\d$/.test(rating)) {
-           var nulled = rating += ".0";
-           return nulled;
+            var nulled = rating += ".0";
+            return nulled;
         };
         return rating;
     }
     function starify(rating) {
         if (rating == undefined) { return; }
         var starryString;
-        starryString = '<i class="fa-solid fa-star"></i>'.repeat(Math.floor(rating)); 
+        starryString = '<i class="fa-solid fa-star"></i>'.repeat(Math.floor(rating));
         if (rating.slice(-2) == ".5") {
             starryString += '<i class="fa-solid fa-star-half"></i>';
         }
@@ -86,13 +86,64 @@ module.exports = async function () {
 
     // Fun facts
     // Double five star episodes
-    var i = returnObj.episodes.length;
-    var fiveStarEps = [];
-    while (i--) {
-        if (epArr[i].ratingJoe == 5 && epArr[i].ratingMike == 5) {
-            fiveStarEps.push(epArr[i])
-        }
-    }
-    returnObj.fiveStars = fiveStarEps.reverse();
+    // var i = returnObj.episodes.length;
+    // var fiveStarEps = [];
+    // while (i--) {
+    //     if (epArr[i].ratingJoe == 5 && epArr[i].ratingMike == 5) {
+    //         fiveStarEps.push(epArr[i])
+    //     }
+    // }
+    returnObj.fiveStars = fiveStarStr(returnObj.episodes);
+    returnObj.mikeAvg = raterAvg(returnObj.episodes)[0];
+    returnObj.joeAvg = raterAvg(returnObj.episodes)[1];
     return returnObj;
+
+    // Fact functions
+    function fiveStarStr(episodes) {
+        // Getting the amount of double 5 star ratings
+        var i = episodes.length;
+        var starEps = 0;
+        var ratingEps = 0;
+        var fiveStarBooks = [];
+        var hs = "<span class='card-hl'>"
+        var he = "</span>"
+        while (i--) {
+            var current = episodes[i];
+            if (current.ratingAve == "5.0") {
+                starEps++;
+                fiveStarBooks.push(current);
+            }
+            if (current.rating) { ratingEps++; }
+        }
+        return `Out of ${ratingEps} ratings, ${hs}${starEps} books${he} have been given a ${hs}double five star rating${he}. That's ${Math.floor(starEps / ratingEps * 100)}%!<br><br>The most recent one to be awarded full stars by both Mike and Joe was ${hs}${fiveStarBooks[fiveStarBooks.length - 1].title}${he}.`;
+    }
+    function raterAvg(episodes) {
+        i = episodes.length;
+        var ratingArr = [];
+        var mikeFives = 0;
+        var joeFives = 0;
+        while (i--) {
+            var c = episodes[i];
+            if (c.rating) {
+                ratingArr.push(c);
+                if (c.ratingMike == "5.0") {
+                    mikeFives++;
+                }
+                if (c.ratingJoe == "5.0") {
+                    joeFives++;
+                }
+            }
+        }
+        var mikeRatingSum = 0;
+        var joeRatingSum = 0;
+        ratingArr.forEach(element => {
+            mikeRatingSum += parseFloat(element.ratingMike);
+            joeRatingSum += parseFloat(element.ratingJoe);
+        });
+        var mikeAvg = mikeRatingSum / ratingArr.length;
+        var joeAvg = joeRatingSum / ratingArr.length;
+        mikeAvg = Math.round(mikeAvg*100) / 100;
+        joeAvg = Math.round(joeAvg*100) / 100;
+    return [mikeAvg, joeAvg];
+    }
 }
