6095575a8ed374abbd6dfbe5188ada04f1480633
cjlm <christian.miles@gmail.com>
2022-07-08 22:17:15 -0700

    New favicon

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/robots.txt');
   eleventyConfig.addPassthroughCopy('src/js/*');
   eleventyConfig.addPassthroughCopy('src/fonts/*');
+  eleventyConfig.addPassthroughCopy('./src/favicon/*');
 
   eleventyConfig.addPassthroughCopy({
     'node_modules/tablesort/dist/tablesort.min.js': 'js/tablesort.min.js',
